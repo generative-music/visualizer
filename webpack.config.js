d23fc4be6267e5f901adb504781a0ecc3e401aa9
metalex9 <alexbainter@gmail.com>
2018-08-23 22:04:26 -0500

    Copy everything over

---
+++ webpack.config.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  plugins: [new HtmlWebpackPlugin({ template: './index.template.html'})]
+};
