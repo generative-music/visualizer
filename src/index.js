fb81c813ade74dbcf3c097235561b521dadfc121
metalex9 <alexbainter@gmail.com>
2019-03-09 15:38:59 -0600

    Use canvas for animation

--- src/index.js
+++ src/index.js
@@ -16,9 +16,9 @@ const getRandomColor = () =>
 
 const canvas = document.getElementById('canvas');
 const shortestDimLength = Math.min(canvas.width, canvas.height);
-const squareLength = Math.floor(Math.sqrt(shortestDimLength ** 2 / 2));
+const squareLength = Math.floor(Math.sqrt(shortestDimLength ** 2 / 2) * 0.98);
 const ctx = canvas.getContext('2d');
-ctx.strokeStyle = 'black';
+ctx.strokeStyle = 'white';
 const lineWidth = Math.max(1, Math.round(squareLength / 100));
 ctx.lineWidth = lineWidth;
 const half = length => length / 2;
@@ -45,13 +45,11 @@ const corners = lineAnchors.filter(
   ([x, y]) => (x === leftX || x === rightX) && (y === topY || y === bottomY)
 );
 
-const lines = lineAnchors.map(coordinates => coordinates.concat([midX, midY]));
-
 const getSingleLinePath = coordinates => {
   const path = new Path2D();
-  coordinates.forEach(([x1, y1, x2, y2]) => {
+  coordinates.forEach(([x1, y1]) => {
     path.moveTo(x1, y1);
-    path.lineTo(x2, y2);
+    path.lineTo(midX, midY);
   });
   return path;
 };
@@ -78,55 +76,139 @@ const trianglePaths = corners.reduce((allTriangles, coordinate) => {
 }, []);
 
 const generateImage = () => {
-  const selectedLines = lines
-    .map((path, index) => ({ path, index }))
+  const selectedAnchors = lineAnchors
+    .map((coordinate, index) => ({ coordinate, index }))
     .filter(() => Math.random() < 0.25);
 
-  const lineIndices = selectedLines.map(({ index }) => index);
+  const anchorIndices = selectedAnchors.map(({ index }) => index);
 
-  const shapeIndices = [];
-  if (lineIndices.length <= 1) {
-    shapeIndices.push([0, 1, 2, 3, 4, 5, 6, 7]);
+  const shapeGroups = [];
+  if (anchorIndices.length <= 1) {
+    shapeGroups.push(trianglePaths);
   } else {
-    for (let i = 0; i < lineIndices.length - 1; i += 1) {
-      const currentIndex = lineIndices[i];
-      const nextIndex = lineIndices[i + 1];
-      const shape = [];
+    for (let i = 0; i < anchorIndices.length - 1; i += 1) {
+      const currentIndex = anchorIndices[i];
+      const nextIndex = anchorIndices[i + 1];
+      const shapeGroup = [];
       for (let j = currentIndex; j < nextIndex; j += 1) {
-        shape.push(j);
+        shapeGroup.push(trianglePaths[j]);
       }
-      shapeIndices.push(shape);
+      shapeGroups.push(shapeGroup);
     }
-    const [firstIndex] = lineIndices;
-    let currentIndex = lineIndices[lineIndices.length - 1];
+    const [firstIndex] = anchorIndices;
+    let currentIndex = anchorIndices[anchorIndices.length - 1];
     const shape = [];
     while (currentIndex !== firstIndex) {
-      shape.push(currentIndex);
+      shape.push(trianglePaths[currentIndex]);
       currentIndex = currentIndex === 7 ? (currentIndex = 0) : currentIndex + 1;
     }
-    shapeIndices.push(shape);
+    shapeGroups.push(shape);
   }
-  ctx.globalCompositeOperation = 'lighter';
+  const selectedShapeGroups = shapeGroups.filter(() => Math.random() < 0.5);
   changeColorScheme();
-
-  shapeIndices.filter(() => Math.random() < 0.5).forEach(shapeIds => {
-    const [r, g, b] = getRandomColor();
-    ctx.fillStyle = `rgba(${r}, ${g}, ${b}, 1)`;
-    shapeIds.forEach(index => {
-      ctx.fill(trianglePaths[index]);
-    });
-  });
-
-  ctx.globalCompositeOperation = 'source-over';
-  const linePath = getSingleLinePath(selectedLines.map(({ path }) => path));
-  ctx.lineCap = 'round';
-  ctx.stroke(linePath);
-  ctx.lineCap = 'butt';
-  ctx.strokeRect(leftX, topY, squareLength, squareLength);
+  return [
+    selectedAnchors.map(({ coordinate }) => coordinate),
+    selectedShapeGroups.map(shapes => ({ shapes, color: getRandomColor() })),
+  ];
 };
 
-generateImage();
-setInterval(() => {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  generateImage();
-}, 2000);
+const animate = () => {
+  let animationStart;
+  const animationTimeMs = 10000;
+  const [anchors, shapeGroups] = generateImage();
+  const drawLines = Math.random() < 0.5;
+
+  const rotate =
+    (anchors.length === 0 || !drawLines) && shapeGroups.length === 0;
+
+  ctx.setTransform(1, 0, 0, 1, 0, 0);
+  if (rotate) {
+    const totalRotation = Math.random() < 0.5 ? 90 : -90;
+    const draw = () => {
+      ctx.setTransform(1, 0, 0, 1, 0, 0);
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+      ctx.translate(midX, midY);
+      const rotationPct = Math.min(
+        (Date.now() - animationStart) / animationTimeMs,
+        1
+      );
+      ctx.rotate(rotationPct * totalRotation * (Math.PI / 180));
+
+      ctx.lineCap = 'butt';
+      ctx.strokeStyle = 'white';
+      ctx.strokeRect(
+        -half(squareLength),
+        -half(squareLength),
+        squareLength,
+        squareLength
+      );
+      if (rotationPct < 1) {
+        window.requestAnimationFrame(() => {
+          draw();
+        });
+      } else {
+        animate();
+      }
+    };
+    window.requestAnimationFrame(() => {
+      animationStart = Date.now();
+      draw();
+    });
+  } else {
+    const linePath = getSingleLinePath(anchors);
+
+    const draw = (fadeIn = true) => {
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+      const now = Date.now();
+      const opacity = fadeIn
+        ? Math.min((now - animationStart) / animationTimeMs, 1)
+        : Math.max(1 - (now - animationStart) / animationTimeMs, 0);
+
+      ctx.globalCompositeOperation = 'lighter';
+
+      shapeGroups.forEach(({ shapes, color }) => {
+        const [r, g, b] = color;
+        const rgbaColor = `rgba(${r}, ${g}, ${b}, ${opacity})`;
+        ctx.fillStyle = rgbaColor;
+        shapes.forEach(shape => {
+          ctx.fill(shape);
+        });
+      });
+
+      ctx.globalCompositeOperation = 'source-over';
+      if (drawLines) {
+        ctx.lineCap = 'round';
+        ctx.strokeStyle = `rgba(255,255,255,${opacity})`;
+        ctx.stroke(linePath);
+      }
+
+      ctx.lineCap = 'butt';
+      ctx.strokeStyle = 'white';
+      ctx.strokeRect(leftX, topY, squareLength, squareLength);
+
+      if (fadeIn && opacity < 1) {
+        window.requestAnimationFrame(() => {
+          draw(true);
+        });
+      } else if (!fadeIn && opacity > 0) {
+        window.requestAnimationFrame(() => {
+          draw(false);
+        });
+      } else if (opacity > 0) {
+        window.requestAnimationFrame(() => {
+          animationStart = Date.now();
+          draw(false);
+        });
+      } else {
+        animate();
+      }
+    };
+
+    window.requestAnimationFrame(() => {
+      animationStart = Date.now();
+      draw();
+    });
+  }
+};
+
+animate();
